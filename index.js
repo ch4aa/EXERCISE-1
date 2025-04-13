1e197d6a91866211308556f7da941d8e99faadfc
natasha <nurnadianatasha03@gmail.com>
2025-04-14 00:20:36 +0800

    task 3

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
 const { MongoClient } = require('mongodb');
 
+    const uri = "mongodb://localhost:27017"
+    const client = new MongoClient (uri);
+
+
 const drivers = [
-    {
-    name: "John Doe",
-    vehicleType: "Sedan",
-    isAvailable: true,
-    rating: 4.8
-    },
-    {
-    name: "Alice Smith", 
-    vehicleType: "SUV",
-    isAvailable: false,
-    rating: 4.5
-    }
+    {name: "John Doe",vehicleType: "Sedan", isAvailable: true, rating: 4.8 },
+    {name: "Alice Smith", vehicleType: "SUV",isAvailable: false, rating: 4.5}, 
+    {name: "Haris J", vehicleType: "honda", isAvailable: true, rating: 4.7}
 ];
 // show the data in the console
 console. log(drivers);
 
 // TODO: show the all the drivers name in the console
-drivers.forEach(driver => {
-    console.log(driver.name); 
-    });
+console.log("Driver Names:");
+drivers.forEach(driver => console.log (driver.name));
+
     
 // TODO: add additional driver to the drivers array
-drivers.push({
-    name: "Bob Johnson",
-    vehicleType: "Truck",
-    isAvailable: true,
-    rating: 4.9
-});
-
-//show the updated list of drivers 
-console.log(drivers);
+drivers.push({ name: "Bob Johnson", vehicleType: "Truck", isAvailable: true, rating: 4.9});
+    console.log("Driver Names:");
+    drivers.forEach(driver => console.log (driver.name));
+    console.log("Added new driver:", driver[drivers.lenght - 1]);
 
 
 async function main() {
-    // Replace <connection-string> with your MongoDB URI
-    const uri = "mongodb://localhost:27017"
-    const client = new MongoClient (uri);
-
     try {
     await client. connect();
     console. log("Connected to MongoDB!");
@@ -46,18 +32,31 @@ async function main() {
     const db = client.db("testDB");
     const collection = db.collection ("users");
 
-    // Insert a document
-    await collection. insertOne({ name: "Alice", age: 25 }); 
-    console. log ("Document inserted!");
+    // Insert drivers 
+    await collection. insertMany(drivers); 
+    console. log ("Drivers inserted!");
 
-    // Query the document
-    const result = await collection. findOne({ name: "Alice" });
-    console. log("Query result:", result);
-    } catch (err) {
-    console.error("Error:", err);
+    // Query high-rated drivers 
+    const highRatedDrivers = await drivercollection. find({
+        rating: {$gte: 4.5}, available: true
+     }).toArray();
+    console. log("High Rated Available Drivers:", highRatedDrivers);
+  
+   //updates John Doe's rating 
+   await drivercollection.updateOne(
+    {name:" John Doe"},
+    {$inc: { rating: 3.1}}
+   );
+console.log("update John Doe's rating 3.1");
+
+//delete unavailable driver
+await driverCollection.deleteMany({available: false});
+console.log ("unavailable drivers deleted!");
+
+    } catch (err){
+        console.error(err);
     } finally {
-    await client.close();
+     await client.close();
     }
-    }
-
+}
     main ();
